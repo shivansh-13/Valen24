fea7cedd9d6175fb57e198c43c93dc31ab811486
Shivansh <shivansh01308@gmail.com>
2024-02-06 13:58:46 +0530

    Fixed bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fc58aef12061514e"; }
+/******/ 		__webpack_require__.h = function() { return "f4a26be2798ef3df"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
