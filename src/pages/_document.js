41faf93ae51e84a627b06c2e0bb637a04a15f902
Shivansh <shivansh01308@gmail.com>
2024-02-12 17:28:24 +0530

    Added seo tags to _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,11 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head >
-      <link
-            href="https://fonts.googleapis.com/icon?family=Material+Icons"
-            rel="stylesheet"
-          />
-          </Head>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+
+        <title>Valentine Ask Out ❤️</title>
+        <meta name="keywords" content="love, crush, ask out, date, valentine, reels, instagram, yes getting bigger" />
+      </Head>
       <body>
         <Main />
         <NextScript />
